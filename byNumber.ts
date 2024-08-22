82c4e0a8cf8f068a18984c1bf88ab591e7ea3080
statiolake <statiolake@gmail.com>
2024-08-22 23:49:54 +0900

    feat: add simplified version (using array length)

---
+++ byNumber.ts
@@ -0,0 +1,55 @@
+type Range<
+  N extends number,
+  Acc extends number[] = [],
+> = Acc["length"] extends N ? Acc : Range<N, [...Acc, Acc["length"]]>;
+
+type _Inc<N extends number> = [...Range<N>, N]["length"];
+type Inc<N extends number> = _Inc<N> extends number ? _Inc<N> : never;
+type IncMod<N extends number, Mod extends number> =
+  Inc<N> extends Mod ? 0 : Inc<N>;
+
+type Memory<
+  N extends number = number,
+  Mod3 extends number = N,
+  Mod5 extends number = N,
+> = [N, Mod3, Mod5];
+
+type IncMemory<C extends Memory> =
+  C extends Memory<infer N, infer Mod3, infer Mod5>
+    ? Memory<Inc<N>, IncMod<Mod3, 3>, IncMod<Mod5, 5>>
+    : never;
+
+type FizzBuzz<C extends Memory> =
+  C extends Memory<infer N, infer Mod3, infer Mod5>
+    ? Mod3 extends 0
+      ? Mod5 extends 0
+        ? "FizzBuzz"
+        : "Fizz"
+      : Mod5 extends 0
+        ? "Buzz"
+        : `${N}`
+    : never;
+
+class FizzBuzzPrinter<F extends Memory = Memory<1>> {
+  print(value: FizzBuzz<F>): FizzBuzzPrinter<IncMemory<F>> {
+    console.log(value);
+    return new FizzBuzzPrinter();
+  }
+}
+
+new FizzBuzzPrinter()
+  .print("1")
+  .print("2")
+  .print("3")
+  .print("4")
+  .print("Buzz")
+  .print("Fizz")
+  .print("7")
+  .print("Buzz")
+  .print("Fiz")
+  .print("10")
+  .print("11")
+  .print("Fizz")
+  .print("13")
+  .print("14")
+  .print("FizzBuzz");
